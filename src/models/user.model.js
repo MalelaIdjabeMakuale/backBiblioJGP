0cc3a7e0921363fd2f39956e831d316b938c6395
bokombe <malela.im90@gmail.com>
2024-04-18 10:27:26 +0200

    version final

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,13 +1,37 @@
 const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema({
-    username: String,
-    name: String,
-    email: String,
-    password: String,
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    surname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+      
+       
+    },
     image: String,
     favorites: [{ type: Schema.Types.ObjectId, ref: 'Book' }],
     read: [{ type: Schema.Types.ObjectId, ref: 'Book' }]
 });
 
 module.exports = model('User', userSchema);
+
+//contraseña minimo 8 caracteres, al menos una minuscula, mayuscula y un digito y un caracter espcial
+
