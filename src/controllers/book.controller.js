a04b3edc298840e62c818a30382d723108d67c16
bokombe <malela.im90@gmail.com>
2024-04-21 11:01:54 +0200

    añadido comentarios

--- src/controllers/book.controller.js
+++ src/controllers/book.controller.js
@@ -1,26 +1,27 @@
+// Importar los módulos necesarios
 const { response } = require("express");
 const Book = require("../models/book.model.js");
-const HTTPSTATUSCODE = require ("../../utils/httpStatusCode.js")
+const HTTPSTATUSCODE = require("../../utils/httpStatusCode.js");
 
- const getBooks = async (request, response) => {
+//Función para obtener todos los libros. Recupera los libros de la base datos y los devuelve en formato JSON.
+const getBooks = async (request, response) => {
   try {
-      const books = await Book.find();
-      response.status(200).json({
-          status: 200,
-          message: HTTPSTATUSCODE[200],
-          data: books
-      });
-
+    const books = await Book.find();
+    response.status(200).json({
+      status: 200,
+      message: HTTPSTATUSCODE[200],
+      data: books,
+    });
   } catch (error) {
-      response.status(404).json({message: HTTPSTATUSCODE[400]});
+    response.status(404).json({ message: HTTPSTATUSCODE[400] });
   }
-} 
+};
 
-// Get 1
+// Función para obtener un libro por ID. Recupera el libro y devuelve un JSON.
 const getBook = async (request, response) => {
   try {
     const id = request.params.id;
-    const book= await Book.findById(id);
+    const book = await Book.findById(id);
     response.status(200).json(book);
   } catch (error) {
     console.log(error.message);
@@ -28,7 +29,7 @@ const getBook = async (request, response) => {
   }
 };
 
-//GetByauthor 
+// Función para obtener un libro por autor. Recupera el libro y devuelve un JSON.
 const getBooksByAuthor = async (request, response) => {
   try {
     const author = request.params.author;
@@ -36,14 +37,16 @@ const getBooksByAuthor = async (request, response) => {
     response.status(200).json({
       status: 200,
       message: HTTPSTATUSCODE[200],
-      data: books
+      data: books,
     });
   } catch (error) {
     console.log(error.message);
-    response.status(404).json({ message: `No se han encontrado libros del autor ${author}` });
+    response
+      .status(404)
+      .json({ message: `No se han encontrado libros del autor ${author}` });
   }
 };
-//getBygenre
+// Función para obtener por género. Recupera el libro y devuelve un JSON.
 const getBooksByGenre = async (request, response) => {
   try {
     const author = request.params.author;
@@ -51,17 +54,17 @@ const getBooksByGenre = async (request, response) => {
     response.status(200).json({
       status: 200,
       message: HTTPSTATUSCODE[200],
-      data: books
+      data: books,
     });
   } catch (error) {
     console.log(error.message);
-    response.status(404).json({ message: `No se han encontrado libros del autor ${genre}` });
+    response
+      .status(404)
+      .json({ message: `No se han encontrado libros del autor ${genre}` });
   }
 };
 
-
-
-// post
+// Función para crear un nuevo libro. Guarda el libro en la base datos y devuelve una respuesta JSON.
 const createBook = async (request, response) => {
   const book = new Book(request.body);
   try {
@@ -76,41 +79,48 @@ const createBook = async (request, response) => {
       .json({ message: `Bad request, compruebe su petición` });
   }
 };
-// patch
+// Función para actualizar el libro. Busca el libro por ID y le introduce nuevos datos.
 const updateBook = async (request, response) => {
   try {
-      const id = request.params.id;
-      const body = request.body;
-      const book = await Book.findByIdAndUpdate(id, body, { new: true });
-      if (!book) {
-          return response.status(404).json({ 
-              status: 404,
-              message: HTTPSTATUSCODE[404], 
-          });
-      }
-      response.status(200).json({
-          status: 200,
-          message: HTTPSTATUSCODE[200],
-          data: book
+    const id = request.params.id;
+    const body = request.body;
+    const book = await Book.findByIdAndUpdate(id, body, { new: true });
+    if (!book) {
+      return response.status(404).json({
+        status: 404,
+        message: HTTPSTATUSCODE[404],
       });
-
+    }
+    response.status(200).json({
+      status: 200,
+      message: HTTPSTATUSCODE[200],
+      data: book,
+    });
   } catch (error) {
-      console.error(error.message);
-      response.status(400).json({ message: error.message });
+    console.error(error.message);
+    response.status(400).json({ message: error.message });
   }
 };
 
-//delete
+//Función para borrar libros. Encuentra el libro por ID y lo borra.
 const deleteBook = async (request, response) => {
-    try {
-      const id = request.params.id;
-      
-      const book = await Book.findByIdAndDelete(id);
-      response.status(200).json({message:'Se borró el libro'});
-    } catch (error) {
-      console.log(error.message);
-      response.status(404).json({ message: error.message});
-    }
-  };
+  try {
+    const id = request.params.id;
 
-  module.exports = {getBooks,getBook, createBook, updateBook, deleteBook, getBooksByAuthor, getBooksByGenre}
+    const book = await Book.findByIdAndDelete(id);
+    response.status(200).json({ message: "Se borró el libro" });
+  } catch (error) {
+    console.log(error.message);
+    response.status(404).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  getBooks,
+  getBook,
+  createBook,
+  updateBook,
+  deleteBook,
+  getBooksByAuthor,
+  getBooksByGenre,
+};
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,115 +1,118 @@
+//Importar módulos necesarios
 const User = require("../models/user.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const HTTPSTATUSCODE = require("../../utils/httpStatusCode");
 
+//Función para crear nuevo usuario. Hacer request, encripta la contraseña con bcrypt y chequea si el mail existe en la base datos. Después guarda el usuario en la base de datos y devuelve un respuesta.
 const createUser = async (req, res, next) => {
-    try {
-        const user = new User(req.body);
-        const saltRounds = 10;
-        user.password = await bcrypt.hash(req.body.password, saltRounds);
+  try {
+    const user = new User(req.body);
+    const saltRounds = 10;
+    user.password = await bcrypt.hash(req.body.password, saltRounds);
 
-        if (await User.findOne({ email: req.body.email })) {
-            return res.status(409).json({
-                status: 409,
-                message: HTTPSTATUSCODE[409],
-                data: null
-            });
-        }
-
-        const userDb = await user.save();
-        
-        return res.status(201).json({
-            status: 201,
-            message: HTTPSTATUSCODE[201],
-            data: null
-        });
-    } catch (error) {
-        next(error);
+    if (await User.findOne({ email: req.body.email })) {
+      return res.status(409).json({
+        status: 409,
+        message: HTTPSTATUSCODE[409],
+        data: null,
+      });
     }
-};
 
+    const userDb = await user.save();
+
+    return res.status(201).json({
+      status: 201,
+      message: HTTPSTATUSCODE[201],
+      data: null,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+//Autentificación de usuario. Encuentra el usuario por el email. Si el usuario y la contraseña coinciden genera un JWT, si no coincide devuelve un error.
 const authenticate = async (req, res, next) => {
-    try {
-        const userInfo = await User.findOne({ email: req.body.email }); 
-        if (userInfo && bcrypt.compareSync(req.body.password, userInfo.password)) {
-            userInfo.password = null;
-            const token = jwt.sign(
-                {
-                    id: userInfo._id,
-                    email: userInfo.email,
-                },
-                req.app.get("secretKey"),
-                { expiresIn: "1d" }
-            );
-            return res.json({
-                status: 200,
-                message: HTTPSTATUSCODE[200],
-                data: { user: userInfo, token: token },
-            });
-        } else {
-            return res.json({
-                status: 400,
-                message: HTTPSTATUSCODE[400],
-                data: null,
-            });
-        }
-    } catch (error) {
-        return next(error);
+  try {
+    const userInfo = await User.findOne({ email: req.body.email });
+    if (userInfo && bcrypt.compareSync(req.body.password, userInfo.password)) {
+      userInfo.password = null;
+      const token = jwt.sign(
+        {
+          id: userInfo._id,
+          email: userInfo.email,
+        },
+        req.app.get("secretKey"),
+        { expiresIn: "1d" }
+      );
+      return res.json({
+        status: 200,
+        message: HTTPSTATUSCODE[200],
+        data: { user: userInfo, token: token },
+      });
+    } else {
+      return res.json({
+        status: 400,
+        message: HTTPSTATUSCODE[400],
+        data: null,
+      });
     }
+  } catch (error) {
+    return next(error);
+  }
 };
-
+//Función para desloguearse. Invalida el token.
 const logout = (req, res, next) => {
-    try {
-        return res.json({
-            status: 200,
-            message: HTTPSTATUSCODE[200],
-            token: null,
-        });
-    } catch (error) {
-        return next(error);
-    }
+  try {
+    return res.json({
+      status: 200,
+      message: HTTPSTATUSCODE[200],
+      token: null,
+    });
+  } catch (error) {
+    return next(error);
+  }
 };
 
+//Función para rescatar todos los usuarios. Encuentra los usuarios en la base de datos y respone con un JSON con los datos de los mismos.
 const getUser = async (req, res, next) => {
-    try {
-        const users = await User.find();
-        res.json({
-            status: 200,
-            message: HTTPSTATUSCODE[200],
-            data: users
-        });
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const users = await User.find();
+    res.json({
+      status: 200,
+      message: HTTPSTATUSCODE[200],
+      data: users,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
+//Busca el usuario por ID y muestra sus datos.
 const getUserById = async (req, res, next) => {
-    try {
-        const userId = req.params.userId;
-        const user = await User.findById(userId);
-        if (!user) {
-            return res.status(404).json({
-                status: 404,
-                message: HTTPSTATUSCODE[404],
-                data: null
-            });
-        }
-        res.json({
-            status: 200,
-            message: HTTPSTATUSCODE[200],
-            data: user
-        });
-    } catch (error) {
-        next(error);
+  try {
+    const userId = req.params.userId;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({
+        status: 404,
+        message: HTTPSTATUSCODE[404],
+        data: null,
+      });
     }
+    res.json({
+      status: 200,
+      message: HTTPSTATUSCODE[200],
+      data: user,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
-
 
 module.exports = {
-    createUser,
-    authenticate,
-    logout,
-    getUser,
-    getUserById
+  createUser,
+  authenticate,
+  logout,
+  getUser,
+  getUserById,
 };
